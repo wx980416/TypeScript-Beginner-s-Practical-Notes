9fabebc9f2d47cb3b8a02f9c29dbe389a1b3b262
wx_orange <wx_orange@orange007.com>
2022-12-07 09:54:29 +0800

    第一次提交

---
+++ 02.basic.1.ts
@@ -0,0 +1,20 @@
+// 字符串
+let s1: string = "1";
+let s2: string = String(1);
+let s3: string = `${1}`;
+
+// 数字
+let n1: number = 1;
+let n2: number = Number("1");
+let n3: number = 0b1111;
+
+// 布尔
+let b1: boolean = true;
+let b2: boolean = Boolean(1);
+
+// symbol
+let sym1: symbol = Symbol();
+let sym2: symbol = Symbol("key");
+
+// bigint
+// let big1: bigint = 100n;
